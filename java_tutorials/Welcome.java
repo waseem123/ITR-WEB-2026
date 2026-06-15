799311b84801a62cafadbacf9b6af27bce66a789
WASEEM ATTAR <waseem_6957@yahoo.com>
2026-06-15 13:23:04 +0530

    Add files via upload

---
+++ java_tutorials/Welcome.java
@@ -0,0 +1,6 @@
+public class Welcome {
+    public static void main(String[] args) {
+        System.out.println("Welcome to java tutorials");
+        System.out.printf("This is a new statement");
+    }
+}
